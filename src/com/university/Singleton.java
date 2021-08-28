16e995d03df8f8fced9544834b441c43fde14400
Shazma <shazmaasad2002@gmail.com>
2021-08-28 16:30:59 +0500

    Initial commit

---
+++ src/com/university/Singleton.java
@@ -0,0 +1,17 @@
+package com.university;
+
+public class Singleton {
+    private static final Singleton singleton = new Singleton( );
+    /* A private Constructor prevents any other
+     * class from instantiating.
+     */
+    private Singleton() { }
+    /* Static 'instance' method */
+    public static Singleton getInstance( ) {
+        return singleton;
+    }
+    /* Other methods protected by singleton-ness */
+    protected static void demoMethod( ) {
+        System.out.println("demoMethod for singleton");
+    }
+}
---
+++ src/com/university/SingletonDemo.java
@@ -0,0 +1,8 @@
+package com.university;
+
+public class SingletonDemo {
+    public static void main(String[] args) {
+        Singleton tmp = Singleton.getInstance( );
+        tmp.demoMethod( );
+    }
+}
